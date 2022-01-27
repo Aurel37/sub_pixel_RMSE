a09328019d5c4520b917bc81105c95f88be97acd
Aurel37 <pion.aurelien@outlook.com>
2022-01-27 22:51:03 +0100

    doc

--- upper_bound.py
+++ upper_bound.py
@@ -9,10 +9,12 @@ def upper_bound(t, mu, K, sigma, sigma_prime):
     if t != 0:
         delta = min(K+1/2 - t, K+1/2+t)
         Delta = max(K+1/2 - t, K+1/2+t)
+        # check if the coefficient are not nul
         if delta + 1/2 == 0 or delta == 0:
             delta = 1
         if Delta == 0 or Delta + 1/2 == 0:
             Delta = 1
+        # upper bund computed in the article of Simon et al.
         upper_bound = (np.sin(np.pi*t)/np.pi)**2*((mu**2 + 4*sigma**2)*(1/(delta + 1/2) + 1/(Delta+1/2))**2 + 2*sigma_prime**2*(1/delta + 1/Delta))
     return upper_bound
 
@@ -24,12 +26,14 @@ def enveloppe_cal(curve, t):
     time = [t[0]]
     pics = [curve[0]]
     n = len(curve)
+    # find the pics of the curves
     for i in range(n-2):
         if curve[i] - curve[i+1] <=0 and curve[i+1] - curve[i+2] >=0:
           pics.append(curve[i+1])
           time.append(t[i+1])
     time.append(t[-1])
     pics.append(curve[-1])
+    # interpolate through these pics to get the  upper envelope
     f = interpolate.interp1d(time, pics)
     res = []
     for i in t:
